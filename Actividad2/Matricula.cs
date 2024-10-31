2b9334057fd5fda7987209ba8d2876d372207040
LUISA FERNANDA PEREZ CARDONA <luisaperez256431@correo.itm.edu.co>
2024-10-30 20:05:21 -0500

    clases

---
+++ Actividad2/Matricula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2
+{
+    public class Matricula
+    {
+        public string Nombre {  get; set; }
+        public int Estrato { get; set; }
+        public int valorMatricula { get; set; }
+
+        public void Inscripcion()
+        {
+            Console.WriteLine("Ingrese el nombre del estudiante");
+        }
+
+    }
+}
