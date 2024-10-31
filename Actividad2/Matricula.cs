028ccf7436ede273b2bfe449505fbc67506463a9
LUISA FERNANDA PEREZ CARDONA <luisaperez256431@correo.itm.edu.co>
2024-10-30 20:29:39 -0500

    METODOS

--- Actividad2/Matricula.cs
+++ Actividad2/Matricula.cs
@@ -10,12 +10,41 @@ namespace Actividad2
     {
         public string Nombre {  get; set; }
         public int Estrato { get; set; }
-        public int valorMatricula { get; set; }
+        public double valorMatricula { get; set; }
+        public double Descuento {  get; set; }
+        public int Bono {  get; set; }  
 
         public void Inscripcion()
         {
-            Console.WriteLine("Ingrese el nombre del estudiante");
+            Console.WriteLine("Ingrese el nombre del estudiante: ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el valor de la matricula: ");
+            valorMatricula = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el estrato del estudiante: ");
+            Estrato = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Liquidacion()
+        {
+            switch (Estrato) {
+                case 1:
+                    Descuento = valorMatricula * 0.80;
+                    Console.WriteLine($"Tiene un descuento por valor de {Descuento}");
+                    break;
+                case 2:
+                    Descuento = valorMatricula * 0.50;
+                    Console.WriteLine($"Tiene un descuento por valor de {Descuento}");
+                    break;
+                case 3:
+                    Descuento = valorMatricula * 0.50;
+                    Console.WriteLine($"Tiene un descuento por valor de {Descuento}");
+                    break;
+
+            }
+
+
         }
 
-    }
+    }  
 }
